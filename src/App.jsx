65701d091c515374bd4c18ae515e56885b4acce4
c4lyp5o <calypso@calypsocloud.one>
2025-10-04 21:36:48 +0800

    fix: use Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
-import { useState } from "react";
-import FileSidebar from "./components/FileExplorer/Sidebar";
-import MediaSidebar from "./components/MediaManager/Sidebar";
-import Viewer from "./components/Viewer";
 import { FileListProvider } from "./context/FileListContext";
+import Main from "./views/Main";
 import "./App.css";
 
 export default function App() {
-	const [appMode, setAppMode] = useState(false);
-
-	const props = {
-		appMode,
-		setAppMode,
-	};
-
 	return (
 		<FileListProvider>
-			<div className="flex h-screen">
-				{!appMode && <FileSidebar {...props} />}
-				{appMode && <MediaSidebar {...props} />}
-				<Viewer />
-			</div>
+			<Main />
 		</FileListProvider>
 	);
 }
