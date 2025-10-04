e7914bf851f42406190dfe10774371c4b737592f
c4lyp5o <calypso@calypsocloud.one>
2025-10-04 18:56:25 +0800

    feat: added context menu

---
+++ src/components/ContextMenu.jsx
@@ -0,0 +1,35 @@
+export default function ContextMenu({
+	appMode,
+	x,
+	y,
+	onClose,
+	onAdd,
+	onRemove,
+}) {
+	return (
+		// biome-ignore lint/a11y/noStaticElementInteractions: <explanation>
+		// biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+		<div
+			className="absolute z-50 w-48 rounded-md border border-slate-300 bg-white shadow-lg"
+			style={{ top: y, left: x }}
+			onClick={onClose} // close when clicking inside
+		>
+			{!appMode && (
+				<button
+					type="button"
+					className="block w-full px-4 py-2 text-left text-xs hover:bg-slate-100"
+					onClick={onAdd}
+				>
+					➕ Add to Media Collection
+				</button>
+			)}
+			<button
+				type="button"
+				className="block w-full px-4 py-2 text-left text-xs hover:bg-slate-100"
+				onClick={onRemove}
+			>
+				❌ Remove from Media Collection
+			</button>
+		</div>
+	);
+}
