8a0a96e990319231ee4e1f910809890b3e0419d6
c4lyp5o <calypso@calypsocloud.one>
2025-10-04 18:57:54 +0800

    fix: made dual mode app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import Sidebar from "./components/Sidebar";
+import { useState } from "react";
+import FileSidebar from "./components/FileExplorer/Sidebar";
+import MediaSidebar from "./components/MediaManager/Sidebar";
 import Viewer from "./components/Viewer";
 import { FileListProvider } from "./context/FileListContext";
 import "./App.css";
 
 export default function App() {
+	const [appMode, setAppMode] = useState(false);
+
+	const props = {
+		appMode,
+		setAppMode,
+	};
+
 	return (
 		<FileListProvider>
 			<div className="flex h-screen">
-				<Sidebar />
+				{!appMode && <FileSidebar {...props} />}
+				{appMode && <MediaSidebar {...props} />}
 				<Viewer />
 			</div>
 		</FileListProvider>
