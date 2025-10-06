8a0422044f8fd3149b9f46e7d4142db2ece4c0db
c4lyp5o <calypso@calypsocloud.one>
2025-10-06 23:44:00 +0800

    fix: fixed sidebar with bottom buttons, change file name for context, add settings page

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,16 +1,23 @@
 import FileSidebar from "../components/FileExplorer/Sidebar";
 import MediaSidebar from "../components/MediaManager/Sidebar";
 import Viewer from "../components/Viewer";
-import { useFileList } from "../context/FileListContext";
+import Settings from "../components/Settings";
+import BottomButtons from "../components/BottomButtons";
+import { useDMFVContext } from "../context/DMFVContext";
 
 export default function Main() {
-	const { appMode } = useFileList();
+	const { appMode, showSettings } = useDMFVContext();
 
 	return (
 		<div className="flex h-screen">
-			{!appMode && <FileSidebar />}
-			{appMode && <MediaSidebar />}
-			<Viewer />
+			<div className="flex h-full w-64 flex-col border-r border-slate-200 bg-slate-50 outline-0">
+				<div className="flex-1 overflow-y-auto">
+					{!appMode && <FileSidebar />}
+					{appMode && <MediaSidebar />}
+				</div>
+				<BottomButtons />
+			</div>
+			{showSettings ? <Settings /> : <Viewer />}
 		</div>
 	);
 }
