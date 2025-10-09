96bec5e90e014510255d9763c72c32434dabcc1f
c4lyp5o <calypso@calypsocloud.one>
2025-10-09 23:05:15 +0800

    fix: use toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { DFMVProvider } from "./context/DFMVContext";
+import { ToastProvider } from "./context/ToastContext";
 import Main from "./views/Main";
 import "./App.css";
 
 export default function App() {
 	return (
 		<DFMVProvider>
-			<Main />
+			<ToastProvider placement="bottom-right">
+				<Main />
+			</ToastProvider>
 		</DFMVProvider>
 	);
 }
